ed297635ac08f0dd95178c618f6b4fe70d05856f
Megan <87830907+MeganWallace@users.noreply.github.com>
2023-09-14 22:52:21 -0400

    Added function to update guest count and close list after 8 guests

--- js/script.js
+++ js/script.js
@@ -11,17 +11,18 @@ const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
 
-//---------- click event for guest button ----------
+//---------- click event for invite button ----------
 addGuestButton.addEventListener("click", function(){
   const guest = guestInput.value; // assigns guest to the input box vale
   // console.log(guest);
   if(guest !== ""){ //checks that input is not empty
     addToList();
     clearInput();
+    updateGuestCount();
   }
 })
 
-//---------- document keydown event for guest button ----------
+//---------- document keydown event for invite button ----------
 document.addEventListener("keydown", function(e){
   if(e.key === "Enter"){
 
@@ -41,4 +42,16 @@ const addToList = function(){
   const listItem = document.createElement("li"); //creates new list item
   listItem.innerText = guest; //assigns guest value to new list item
   guestList.append(listItem); //adds new list item to guest list
+}
+
+//---------- function to update guest count ----------
+const updateGuestCount = function(){
+  const guests = document.querySelectorAll(".guest-list li") //selects all list items
+  guestCount.innerText = guests.length
+  if(guests.length>=8){
+    addGuestButton.classList.add("hide"); //hides invite button
+    guestInput.classList.add("hide"); //hides text box
+    guestInputLabel.classList.add("hide"); //hides invite button label
+    guestFull.classList.remove("hide"); //shows guest list full message
+  }
 }
